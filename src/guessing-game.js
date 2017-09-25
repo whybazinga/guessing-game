301c9647132843634d682fe1bbd022b4de43148c
Alex Muharski <aom9@mail.ru>
2017-09-26 01:05:15 +0300

    Final commit(hope so)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.cMin = min;
+        this.cMax = max;
     }
 
     guess() {
-
+        return Math.ceil((this.cMin + this.cMax) / 2);
     }
 
     lower() {
-
+        this.setRange(this.cMin , Math.ceil((this.cMin + this.cMax) / 2));
     }
 
     greater() {
-
+        this.setRange(Math.ceil((this.cMin + this.cMax) / 2) , this.cMax);
     }
 }
 
